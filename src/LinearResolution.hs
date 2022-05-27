fd7ab18853b89cb3b03b390a594d1873fe119b41
Mistral Contrastin <me@madgen.net>
2022-05-27 17:30:51 +0100

    Use a stack of goals

--- src/LinearResolution.hs
+++ src/LinearResolution.hs
@@ -2,17 +2,21 @@ module LinearResolution where
 
 import AST
 
-resolve :: [ Atom ] -> Clause -> Maybe [ Atom ]
+type GoalStack = [ [ Atom ] ]
+
+resolve :: GoalStack -> Clause -> Maybe GoalStack
 resolve [] _ = error "Why are you resolving?"
-resolve (p : ps) (p' :- qs)
-  | p == p' = Just (qs <> ps)
+resolve ([] : _) _ = error "Why are you resolving?"
+resolve ((p : ps) : pss) (q :- qs)
+  | p == q = Just (qs : ps : pss)
   | otherwise = Nothing
 
 derive :: [ Clause ] -> Atom -> Bool
-derive originalClauses query = go [ query ] Nothing originalClauses
+derive originalClauses query = go [ [ query ] ] Nothing originalClauses
   where
-  go :: [ Atom ] -> Maybe Clause -> [ Clause ] -> Bool
+  go :: GoalStack -> Maybe Clause -> [ Clause ] -> Bool
   go [] _ _ = True
+  go ([] : goals) active clauses = go goals active clauses
   go goals Nothing (clause : clauses) = go goals (Just clause) clauses
   go goals (Just clause) clauses =
     case resolve goals clause of
