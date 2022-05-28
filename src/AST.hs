6be7aa746ec6326a00704f8a3de8276418f9da0e
Mistral Contrastin <me@madgen.net>
2022-05-28 23:29:56 +0100

    Add terms to AST

--- src/AST.hs
+++ src/AST.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE GeneralisedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 module AST where
 
 import qualified Data.Text as T
-import Data.String (IsString)
+import Data.String (IsString(..))
 
 data Clause = Head :- Body
 
@@ -11,9 +13,19 @@ type Head = Atom
 
 type Body = [ Atom ]
 
-newtype Atom = Atom Name deriving (Eq, IsString)
+data Atom = Atom Name [ Term ] deriving Eq
+
+data Term = Lit Name | Var Name deriving (Eq, Ord)
 
 type Name = T.Text
 
 instance Show Atom where
-  show (Atom name) = T.unpack name
+  show (Atom name terms) =
+    T.unpack (name <> "(" <> T.intercalate "," (map (T.pack . show) terms) <> ")")
+
+instance Show Term where
+  show (Lit name) = T.unpack ("\"" <> name <> "\"")
+  show (Var name) = T.unpack name
+
+instance IsString Atom where
+  fromString name = Atom (T.pack name) []
