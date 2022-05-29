6cc82b156a23ac6d73ce3085914117c2002f5e7b
Mistral Contrastin <me@madgen.net>
2022-05-29 22:11:35 +0100

    Add query binding resolution

--- src/LinearResolution.hs
+++ src/LinearResolution.hs
@@ -15,15 +15,23 @@ resolve env ((p : ps) : pss) (q :- qs)
   | Just env <- unify env p q = Just (env, qs : ps : pss)
   | otherwise = Nothing
 
-derive :: [ Clause ] -> Atom -> Bool
-derive originalClauses query = go 0 P.empty [ [ query ] ] Nothing originalClauses
+derive :: [ Clause ] -> Atom -> Maybe Atom
+derive originalClauses query =
+    case go 0 P.empty [ [ query ] ] Nothing originalClauses of
+      Just env -> Just (subst env query)
+      Nothing -> Nothing
   where
-  go :: Int -> Env -> GoalStack -> Maybe Clause -> [ Clause ] -> Bool
-  go _ _ [] _ _ = True
+  go :: Int -> Env -> GoalStack -> Maybe Clause -> [ Clause ] -> Maybe Env
+  go _ env [] _ _ = Just env
   go i env ([] : goals) active clauses = go i env goals active clauses
   go i env goals Nothing (clause : clauses) = go i env goals (Just clause) clauses
   go i env goals (Just clause) clauses =
     case resolve env goals (nameClause i clause) of
-      Just (env', goals') | go (i + 1) env' goals' Nothing originalClauses -> True
+      Just (env', goals')
+        | Just env'' <- go (i + 1) env' goals' Nothing originalClauses ->
+          Just env''
       _ -> go i env goals Nothing clauses
-  go _ _ _ _ [] = False
+  go _ _ _ _ [] = Nothing
+
+subst :: Env -> Atom -> Atom
+subst env (Atom name terms) = Atom name (map (P.rep env) terms)
--- test/LinearResolutionSpec.hs
+++ test/LinearResolutionSpec.hs
@@ -2,17 +2,20 @@
 
 module LinearResolutionSpec where
 
+import Data.Maybe (isJust)
+
+import Test.Hspec
+
 import AST
 import LinearResolution
 
-import Test.Hspec
 
 spec :: Spec
 spec =
   describe "LinearResolution" $ do
     describe "p :- q, r. q :- r. r." $ do
       let pr = [ "p" :- ["q", "r"], "q" :- [ "r" ], "r" :- [] ]
-      let pred = derive pr
+      let pred = isJust . derive pr
       it "p" $ "p" `shouldSatisfy` pred
       it "q" $ "q" `shouldSatisfy` pred
       it "r" $ "r" `shouldSatisfy` pred
@@ -20,7 +23,7 @@ spec =
 
     describe "p :- q, r. q :- s. q :- r. r." $ do
       let pr = [ "p" :- ["q", "r"], "q" :- [ "s" ], "q" :- [ "r" ], "r" :- [] ]
-      let pred = derive pr
+      let pred = isJust . derive pr
       it "p" $ "p" `shouldSatisfy` pred
       it "q" $ "q" `shouldSatisfy` pred
       it "r" $ "r" `shouldSatisfy` pred
@@ -28,7 +31,7 @@ spec =
 
     describe "p :- q, r. q :- s. q :- r." $ do
       let pr = [ "p" :- ["q", "r"], "q" :- [ "s" ], "q" :- [ "r" ] ]
-      let pred = derive pr
+      let pred = isJust . derive pr
       it "not p" $ "p" `shouldNotSatisfy` pred
       it "not q" $ "q" `shouldNotSatisfy` pred
       it "not r" $ "r" `shouldNotSatisfy` pred
@@ -46,14 +49,18 @@ spec =
             , adviser "Andy Hopper" "Andy Rice" :- []
             , adviser "David Wheeler" "Andy Hopper" :- []
             ]
-      let pred = derive pr
+      let pred = isJust . derive pr
       it "ancestor('Andy Rice', 'Mistral Contrastin')" $
         ancestor "Andy Rice" "Mistral Contrastin" `shouldSatisfy` pred
       it "ancestor('David Wheeler', 'Mistral Contrastin')" $
         ancestor "David Wheeler" "Mistral Contrastin" `shouldSatisfy` pred
       it "ancestor('Alan Mycroft', 'Mistral Contrastin')" $
         ancestor "Alan Mycroft" "Mistral Contrastin" `shouldSatisfy` pred
-      it "not ancestor('Alan Mycroft', 'Andrew Rice)" $
+      it "not ancestor('Alan Mycroft', 'Andrew Rice')" $
         ancestor "Alan Mycroft" "Andrew Rice" `shouldNotSatisfy` pred
-      it "not ancestor('Alan Mycroft', 'Alan Mycroft)" $
+      it "not ancestor('Alan Mycroft', 'Alan Mycroft')" $
         ancestor "Alan Mycroft" "Alan Mycroft" `shouldNotSatisfy` pred
+
+      let pred = derive pr
+      it "ancestor('Andy Rice, ?X)" $
+        pred (ancestor "Andy Rice" "?X") `shouldBe` Just (ancestor "Andy Rice" "Mistral Contrastin")
