faa7d7bf2bb01a0a417a26bd803a05e72f67f82d
Mistral Contrastin <me@madgen.net>
2022-06-03 01:47:20 +0100

    Get rid of unnecessary 'focus' state

--- src/Language/Protolog/LinearResolution.hs
+++ src/Language/Protolog/LinearResolution.hs
@@ -21,7 +21,7 @@ resolve env ((p : ps) : pss) (q :- qs)
 
 derive :: forall p. Provenance p => [ Clause ] -> Atom -> Maybe (Env, p)
 derive originalClauses query =
-  go 0 P.empty queryGoalStack Nothing originalClauses queryProvenance
+  go 0 P.empty queryGoalStack originalClauses queryProvenance
   where
   queryGoalStack = [ [ query ] ]
   queryProvenance = unit queryGoalStack
@@ -29,29 +29,27 @@ derive originalClauses query =
   go :: Int
      -> Env
      -> GoalStack
-     -> Maybe Clause
      -> [ Clause ]
      -> p
      -> Maybe (Env, p)
-  go _ env [] _ _ pt = Just (env, pt)
-  go i env ([] : goals) active clauses pt = go i env goals active clauses pt
-  go i env goals Nothing (clause : clauses) pt = go i env goals (Just clause) clauses pt
-  go i env goals (Just clause) clauses pt =
+  go _ env [] _ pt = Just (env, pt)
+  go i env ([] : goals) clauses pt = go i env goals clauses pt
+  go i env goals (clause : clauses) pt =
     case resolve env goals namedClause of
       Just (env', goals') | pt' <- connect pt goals' namedClause ->
-        go (i + 1) env' goals' Nothing originalClauses pt'
+        go (i + 1) env' goals' originalClauses pt'
         <|>
         -- Despite resolution succeeding right now, derivation fails somewhere
         -- down the line, try the next clause discarding the additional
         -- unifications added to the environment. In other words, we
         -- _backtrack_ on failure.
-        go i env goals Nothing clauses pt
+        go i env goals clauses pt
       _ ->
         -- Resolution with the current clause failed, try the next one.
-        go i env goals Nothing clauses pt
+        go i env goals clauses pt
     where
     namedClause = nameClause i clause
-  go _ _ _ _ [] _ = Nothing
+  go _ _ _ [] _ = Nothing
 
 run :: [ Clause ] -> Atom -> Maybe Atom
 run originalClauses query =
